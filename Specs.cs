f11a38fd4155b29781bf3afe39d089e72b09238c
vardhan0009 <jyothivardhanarao@usf.edu>
2024-03-06 12:18:06 -0500

    Repo2

---
+++ Specs.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehicle
+{
+    internal class Specs:Engine
+    { 
+
+
+            public string BodyStyle;
+            public double WheelBase;
+            public string Headlights;
+
+
+            public Specs(string bodyStyle, double wheelBase, string headlights)
+            {
+
+                BodyStyle = bodyStyle;
+                WheelBase = wheelBase;
+                Headlights = headlights;
+            }
+
+
+            public void DisplayExteriorSpecifications()
+            {
+
+                Console.WriteLine($"Body Style: {BodyStyle}");
+                Console.WriteLine($"Wheelbase: {WheelBase} inches");
+                Console.WriteLine($"Headlights: {Headlights}");
+            }
+        }
+    }
