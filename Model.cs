447a9ccd80e7d1dfb15ce670750769be256f70a5
rohann160 <rohanpatel22@usf.edu>
2024-03-06 12:22:23 -0500

    Repo3

---
+++ Model.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehicle
+{
+    internal class Model
+    {
+        public string CarName;
+        public string ModelYear;
+
+        public Model(string cName, string mYear)
+        {
+
+            CarName = cName;
+            this.ModelYear = mYear;
+        }
+        public string getName()
+        {
+            return CarName + " " + ModelYear;
+        }
+    }
+}
